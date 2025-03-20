c9192dab4a0b354961a487c0acc670543dde5ebf
Harley <harley@corbalt.com>
2025-03-19 21:06:25 -0700

    adjust the jwk api endpoint

--- app/api/jwks.json/route.ts
+++ app/api/jwks.json/route.ts
@@ -1,29 +1,12 @@
-// app/api/auth/route.ts
-import { type NextRequest } from 'next/server';
-import { getOAuthClient } from '@/app/lib/oauth-client';
+import { getOAuthClient } from "@/app/lib/oauth-client"
 
-export const runtime = 'nodejs'; // Force Node.js runtime
-
-export async function GET(request: NextRequest) {
-  try {
-    const client = await getOAuthClient();
-    const sessionCookie = request.cookies.get('bsky-session')?.value;
-
-    if (!sessionCookie) {
-      return new Response(JSON.stringify({ session: false }), {
-        status: 401,
-        headers: { 'Content-Type': 'application/json' }
-      });
-    }
-
-    const session = await client.restore(sessionCookie);
-    return Response.json({ session: !!session });
-    
-  } catch (error) {
-    console.error('Error:', error);
-    return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
-      status: 500,
-      headers: { 'Content-Type': 'application/json' }
-    });
-  }
+export async function GET() {
+  const client = await getOAuthClient()
+  const response = Response.json(client.jwks, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+  console.log(response)
+  return response
 }
